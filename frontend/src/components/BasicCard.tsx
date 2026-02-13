216890611bab8787b7204ab4acfa955d6a82f76e
Diggen85 <17568838+Diggen85@users.noreply.github.com>
2026-02-13 18:21:49 +0100

    WIP better Baselayout and BasicCard CSS

--- frontend/src/components/BasicCard.tsx
+++ frontend/src/components/BasicCard.tsx
@@ -13,18 +13,16 @@ export type BasicCardProps = {
 export default function BasicCard(props: BasicCardProps) {
 
   return (
-    <div className="max-w-md rounded-2xl bg-gray-50 p-4 shadow-xl">
-      <div className="flex flex-row">
-        <div className="mb-6 flex flex-grow items-center gap-4">
+    <div className="rounded-2xl bg-gray-50 p-4 shadow-xl flex flex-col">
+      <div className="flex flex-row mb-4 gap-4 items-center">
           <div className="flex h-12 w-12 items-center justify-center rounded-lg bg-gray-100">
             {props.icon}
           </div>
-          <div className="flex-row h-12 content-center">
+          <div className="h-12 content-center flex flex-col flex-grow">
           <h2 className="text-xl font-semibold text-gray-800 ">{props.headline}</h2>
           {props.subline && (<h2 className="text-xs font-semibold text-gray-600">{props.subline}</h2>)}
           </div>
-        </div>
-        <div className="mb-6 flex flex-grow-0">
+        <div className="items-top flex flex-grow-0">
           {props.action}
         </div>
       </div>
